4d486a4021f6b943efca2898ff5b84a0023b8fba
Thomas Joseph <33921393+thomasjoseph007@users.noreply.github.com>
2022-10-20 22:36:03 +0530

    Update variable.tf

--- AWS-Users/variable.tf
+++ AWS-Users/variable.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-  default = "AKIA24ARHYD5DTG5LQ5N"
+  default = "<access key from IAM user>"
 }
 variable "secret_key" {
-  default = "hrBJF8uyeJuf986U/qL68pAB2WExChSkgAvAAkY5"
+  default = "<secret key from IAM user>"
 }
 variable "region" {
   default = "ap-south-1"
