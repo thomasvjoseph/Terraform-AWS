5079a21fd65a8bf1d8e6e67089b103c6f80ce469
Thomas Joseph <33921393+thomasvjoseph@users.noreply.github.com>
2023-02-08 10:43:50 +0530

    changes

--- EC2-Keypair/variable.tf
+++ EC2-Keypair/variable.tf
@@ -1,8 +1,8 @@
 variable "access_key" {
-  default = "AKIA24ARHYD5PJAEL3FB"
+  default = " "
 }
 variable "secret_key" {
-  default = "KI91SB7ER5KJTB39n21Fo3Ao+ZRmQ10FTvjbe161"
+  default = " "
 }
 variable "region" {
   default = "ap-south-1"
