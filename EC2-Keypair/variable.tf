bfb74934a009878a0f2f76edf005510c5ff5ba60
Thomas Joseph <33921393+thomasvjoseph@users.noreply.github.com>
2023-02-08 10:42:51 +0530

    Add files via upload

---
+++ EC2-Keypair/variable.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  default = "AKIA24ARHYD5PJAEL3FB"
+}
+variable "secret_key" {
+  default = "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"
+}
+variable "region" {
+  default = "ap-south-1"
+}
