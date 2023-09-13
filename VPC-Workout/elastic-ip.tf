4858d694f2a36dc6bbbfc57305772a6be3b30a44
Thomas Joseph <33921393+thomasvjoseph@users.noreply.github.com>
2023-09-13 15:23:58 +0530

    Update elastic-ip.tf

--- VPC-Workout/elastic-ip.tf
+++ VPC-Workout/elastic-ip.tf
@@ -1,11 +1,14 @@
 resource "aws_eip" "one" {
-  vpc                       = true
+  domain                       = "vpc"
   network_interface         = aws_network_interface.n-interface.id
   associate_with_private_ip = "10.0.1.50"
   depends_on = [
     aws_internet_gateway.i-gw
   ]
 }
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.ec2-web.id
+  allocation_id = aws_eip.one.id
+}
 output "server_public_ip" {
    value = aws_eip.one.public_ip
-}
